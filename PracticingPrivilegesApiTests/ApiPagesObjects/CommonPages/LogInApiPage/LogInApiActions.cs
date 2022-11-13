ca8d3d507dacbed95f22a4f7c2d77e238beb4a80
Максим Перевалов <nnpctonsp@gmail.com>
2022-11-13 16:49:03 +0200

    13.11.2022 16 : 00

--- PracticingPrivilegesApiTests/ApiPagesObjects/CommonPages/LogInApiPage/LogInApiActions.cs
+++ PracticingPrivilegesApiTests/ApiPagesObjects/CommonPages/LogInApiPage/LogInApiActions.cs
@@ -47,5 +47,30 @@ namespace PracticingPrivilegesApiTests.ApiPagesObjects.LogInApiPage
 
             return dtoObject;
         }
+
+        //EXAMPLE:
+        public static ResponseLogIn PutsBox(string email, string password)
+        {
+            var restClient = new RestClient(EndPointsApi.apiHost);
+
+            var restRequest = new RestRequest($"https://preview.putsbox.com/p/{email}/last.text", Method.Get);
+
+            restRequest.AddHeaders(Headers.HeadersCommon());
+
+            restRequest.AddJsonBody(RequestBody(email, password));
+
+            var response = restClient.Execute(restRequest);
+
+            var content = response.Content;
+
+            if (response.StatusDescription == "Bad Request")
+            {
+                Console.WriteLine(response.Content);
+            }
+
+            var dtoObject = JsonConvert.DeserializeObject<ResponseLogIn>(content);
+
+            return dtoObject;
+        }
     }
 }
